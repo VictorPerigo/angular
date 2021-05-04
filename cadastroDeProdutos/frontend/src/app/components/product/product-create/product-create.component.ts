34a8d6f4e51c75381ed8adcb8260566b6e73500e
VictorPerigo <victorperigo@gotmail.com>
2021-05-04 16:48:54 -0300

    conection db

--- cadastroDeProdutos/frontend/src/app/components/product/product-create/product-create.component.ts
+++ cadastroDeProdutos/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from '../product.service';
 import {Router} from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
+
+
 export class ProductCreateComponent implements OnInit {
 
+  product: Product={
+    name: 'teste',
+    price: 125.98
+  }
   constructor(
     private productService: ProductService,
     private router: Router,
@@ -19,7 +26,11 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void {
-    this.productService.showMessage('produto criado')
+    this.productService.create(this.product).subscribe(()=> {
+      this.productService.showMessage('produto criado')
+      this.router.navigate(['/products'])
+    })
+
   }
   
   cancel(): void {
